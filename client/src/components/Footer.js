26852ce68b566a01347abaf6b8cc45b83b671df1
OmniOmni <austin.olijar@gmail.com>
2022-07-11 21:29:19 -0500

    logo added

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,8 +3,18 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="h-36 w-screen bg-gray-900 grid grid-cols-3 grid-rows-4 px-5">
-      <div className="border row-span-2 row-start-2 col-start-1 col-span-1 flex justify-center items-center text-3xl">
-        <Link to="/"><h2 className="text-center">Logo</h2></Link>
+      <div className="row-span-2 row-start-2 col-start-1 col-span-1 flex justify-center items-center text-2xl">
+        <Link
+          className="pl-5 w-fit h-full cursor-pointer flex items-center font-thin"
+          to="/"
+        >
+          <img
+            className="h-full"
+            alt="Borrow A Pencil Logo"
+            src="./imgs/logo.svg"
+          />{" "}
+          <h2 className="ml-5 pr-5">Borrow A Pencil</h2>
+        </Link>
       </div>
       <div className="font-thin col-start-2 row-span-4  flex justify-around items-center flex-col">
         <Link className="" to="/about">
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,34 +1,79 @@
-import { Link, useLocation } from "react-router-dom"
-import {useState, useEffect }from 'react'
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [aboutLocation, setAboutLocation] = useState(false);
 
-    const [aboutLocation, setAboutLocation] = useState(false)
+  const location = useLocation();
 
-    const location = useLocation()
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      setAboutLocation(true);
+      return;
+    }
+    setAboutLocation(false);
+  }, [location]);
 
-    useEffect(() => {
-        if (location.pathname !== '/') {
-            setAboutLocation(true);
-            return;
-        }
-        setAboutLocation(false)
-    }, [location])
+  return (
+    <header className="h-24 flex items-center w-full bg-black ">
+      <div className="ml-10 w-3/5 h-full p-2">
+        <Link className="pl-5 w-fit h-full cursor-pointer flex items-center font-thin" to="/">
+          <img className="h-full" alt="Borrow A Pencil Logo" src="./imgs/logo.svg" />{" "}
+          <h2 className="ml-5 pr-5">Borrow A Pencil</h2>
+        </Link>
+      </div>
 
+      <nav className="w-2/5 h-full flex items-center float-right ">
+        <ul className="flex justify-around font-thin mx-10 w-full h-full">
+          <li className="cursor-pointer grow flex items-center justify-center group">
+            {aboutLocation ? (
+              <Link
+                className="group-hover:underline grow flex h-full items-center justify-center"
+                to="/about"
+              >
+                About
+              </Link>
+            ) : (
+              <a
+                className="group-hover:underline grow flex h-full items-center justify-center"
+                target="_self"
+                href="#about-home"
+              >
+                About
+              </a>
+            )}
+          </li>
+          <li className="cursor-pointer grow flex items-center justify-center group">
+            <Link
+              className="group-hover:underline grow flex h-full items-center justify-center"
+              to="/borrow"
+            >
+              Borrow
+            </Link>
+          </li>
+          <li className="cursor-pointer grow flex items-center justify-center group">
+            <Link
+              className="group-hover:underline grow flex h-full items-center justify-center"
+              to="/offer"
+            >
+              Offer
+            </Link>
+          </li>
+          <li className="cursor-pointer grow flex items-center justify-center group">
+            <Link
+              className="group-hover:underline grow flex h-full items-center justify-center"
+              to="/ask"
+            >
+              Ask
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="mr-10 cursor-pointer hover:underline h-full terxt-center w-fit flex items-center justify-center">
+        Account
+      </div>
+    </header>
+  );
+};
 
-    return (
-    <header className="h-20 flex items-center w-full bg-black">
-        <div className="float-left ml-10 w-3/5 cursor-pointer"><Link to="/">Logo</Link></div>
-        <nav className="w-2/5 h-full flex items-center float-right">
-            <ul className="flex justify-around font-thin mx-10 w-full h-full">
-                <li className="cursor-pointer grow flex items-center justify-center group">{aboutLocation ? <Link className="group-hover:underline grow flex h-full items-center justify-center" to="/about">About</Link> : <a className="group-hover:underline grow flex h-full items-center justify-center" target="_self" href="#about-home">About</a>}</li>
-                <li className="cursor-pointer grow flex items-center justify-center group"><Link className="group-hover:underline grow flex h-full items-center justify-center" to="/borrow">Borrow</Link></li>
-                <li className="cursor-pointer grow flex items-center justify-center group"><Link className="group-hover:underline grow flex h-full items-center justify-center" to="/offer">Offer</Link></li>
-                <li className="cursor-pointer grow flex items-center justify-center group"><Link className="group-hover:underline grow flex h-full items-center justify-center" to="/ask">Ask</Link></li>
-            </ul>
-        </nav>
-        <div className="mr-10 cursor-pointer hover:underline">Account</div>
-    </header>)
-}
-
-export default Header
+export default Header;
