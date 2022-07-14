a6709b35c847cba6f211d075b4d33ee86e9774bf
OmniOmni <austin.olijar@gmail.com>
2022-07-14 18:56:41 -0500

    header hove effects complete

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,9 +17,9 @@ const Header = () => {
   return (
     <header className="h-24 flex items-center w-full bg-black ">
       <div className="ml-10 w-3/5 h-full p-2 ">
-        <Link className="pl-5  w-fit h-full group  cursor-pointer flex items-center font-thin" to="/">
-          <img className="h-full" alt="Borrow A Pencil Logo" src="./imgs/logo.svg" />{" "}
-          <h2 className="ml-5 pr-5 before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 hover:before:opacity-100 after:opacity-100 hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-[0px] hover:after:w-[109px] after:bottom-0 after:right-[19px]  after:transition-[width] after:absolute after:bg-white hover:before:w-[109px] relative before:h-[2px] before:bottom-0 ">Borrow A Pencil</h2>
+        <Link className="px-5 w-fit h-full group  cursor-pointer flex items-center font-thin" to="/">
+          <img className="h-full " alt="Borrow A Pencil Logo" src="./imgs/logo.svg" />{" "}
+          <h2 className="ml-5 before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0 ">Borrow A Pencil</h2>
         </Link>
       </div>
 
@@ -28,10 +28,10 @@ const Header = () => {
           <li className="cursor-pointer grow flex items-center justify-center group">
             {aboutLocation ? (
               <Link
-                className="group-hover:underline underline-offset-2 grow flex h-full items-center justify-center"
+                className="grow flex h-full items-center justify-center"
                 to="/about"
               >
-                About
+                <p className="before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0">About</p>
               </Link>
             ) : (
               <a
@@ -39,39 +39,45 @@ const Header = () => {
                 target="_self"
                 href="#about-home"
               >
-                About
+                <p className="before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0">About</p>
               </a>
             )}
           </li>
           <li className="cursor-pointer grow flex items-center justify-center group">
             <Link
-              className="group-hover:underline underline-offset-2 grow flex h-full items-center justify-center"
+              className="grow flex h-full items-center justify-center"
               to="/borrow"
             >
-              Borrow
+              <p className="before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0">Borrow</p>
             </Link>
           </li>
           <li className="cursor-pointer grow flex items-center justify-center group">
             <Link
-              className="group-hover:underline underline-offset-2 grow flex h-full items-center justify-center"
+              className="grow flex h-full items-center justify-center"
               to="/offer"
             >
-              Offer
+              <p className="before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0">Offer</p>
             </Link>
           </li>
           <li className="cursor-pointer grow flex items-center justify-center group">
             <Link
-              className="group-hover:underline underline-offset-2 grow flex h-full items-center justify-center"
+              className="grow flex h-full items-center justify-center"
               to="/ask"
             >
-              Ask
+              <p className="before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0">Ask</p>
+            </Link>
+          </li>
+          <li className="cursor-pointer grow flex items-center justify-center group">
+            <Link
+              className="grow flex h-full items-center justify-center "
+              to="/account"
+            >
+              <p className="font-normal before:absolute before:w-0 before:bg-white before:transition-[width] before:opacity-0 group-hover:before:opacity-100 after:opacity-100 group-hover:after:opacity-0 before:duration-500 after:duration-500 before:origin-left before:left-0 after:origin-right after:h-[2px] after:w-0 group-hover:after:w-full after:bottom-0 after:right-0  after:transition-[width] after:absolute after:bg-white group-hover:before:w-full relative before:h-[2px] before:bottom-0">Account</p>
             </Link>
           </li>
         </ul>
       </nav>
-      <div className="mr-10 cursor-pointer hover:underline underline-offset-2 h-full terxt-center w-fit flex items-center justify-center">
-        Account
-      </div>
+   
     </header>
   );
 };
