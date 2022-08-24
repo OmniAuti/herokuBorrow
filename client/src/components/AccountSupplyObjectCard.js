6e25469c230994f8ef43d3f82b276a1f8e051235
OmniOmni <austin.olijar@gmail.com>
2022-08-24 07:51:43 -0500

    delete button modified

--- client/src/components/AccountSupplyObjectCard.js
+++ client/src/components/AccountSupplyObjectCard.js
@@ -143,7 +143,7 @@ const AccountSupplyObjectCard = ({ data, modalDispatch }) => {
             alt="Edit Post Icon"
           />
         </button>
-        <button className="bg-white text-center cursor-pointer absolute -right-2 -bottom-2 border p-2 hover:shadow-[0_-5px_10px_1px_rgb(0,0,0,0.05),0_-5px_6px_1px_rgb(0,0,0,0.05)] rounded-lg">
+        <button className="bg-white text-center cursor-pointer absolute -right-2 -bottom-2 border p-2 hover:shadow-[0px_-4px_15px_-3px_rgb(0,0,0,0.1),0px_-3px_6px_-4px_rgb(0,0,0,0.1)] rounded-lg">
         <img
             className="w-10 mx-auto mb-1 "
             src="./imgs/deletePost.svg"
