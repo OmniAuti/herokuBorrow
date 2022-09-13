d9248fe3d9b5d8a9bda6036e6ad0a8332a1fd1db
OmniOmni <austin.olijar@gmail.com>
2022-09-12 21:46:57 -0500

    first

--- server/server.js
+++ server/server.js
@@ -4,18 +4,24 @@ const server = express();
 const router = require("./routes/routes");
 const notFound = require("./middleware/route-not-found");
 // DATABASE
-const connectDB = require('./db/connectDB')
+const connectDB = require("./db/connectDB");
 require("dotenv").config();
 const cors = require("cors");
-
+const path = require("path");
 //MIDDLEWARE
+server.use(express.static(path.join(__dirname, "build")));
 server.use(cors());
 server.use(express.json());
 server.use("/api/v1/items", router);
 server.use(express.urlencoded({ extended: true }));
 server.use(notFound);
 
-const port = 2000;
+
+server.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+const port = process.env.PORT || 2000;
 
 const start = async () => {
   try {
