ae7a18af0719881ead0e1152020a56aca6d9e70c
Austin Olijar <Austin.Olijar@gmail.com>
2022-09-13 09:28:49 -0500

    Update route-not-found.js

--- middleware/route-not-found.js
+++ middleware/route-not-found.js
@@ -1,6 +1,3 @@
-const notFound = (req, res) => {
-  if (res.status === 404) {
-    res.status(404).send("This Route Was Not Found");
-  }
-};
-module.exports = notFound;
+const notFound = (req,res) => res.status(404).send('This Route Was Not Found')
+
+module.exports = notFound
